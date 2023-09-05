b999749a4ad6aafc947f8d5c05e29f6ea6f44196
Vitaliy Protasevych <vitaliy.protasevych@gmail.com>
2023-09-05 18:51:22 +0300

    Adaptive
    need to fix second slide for adaptive

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 const sliderLine = document.querySelector('.slider__line');
 const sliderImages = Array.from(sliderLine.children);
 const sliderDots = document.querySelectorAll('.dot');
+const slider = document.querySelector('.slider')
 
 let position = 0,
    dotIndex = 0;
+
+window.addEventListener('resize', showSlide)
+function showSlide() {
+   sliderWidth = document.querySelector('.slider').offsetWidth;
+   sliderLine.style.width = sliderWidth * sliderImages.length + 'px';
+   sliderImages.forEach(image => image.style.width = sliderWidth + 'px')
+}
 
 const thisSlide = (index) => {
    for (let dot of sliderDots) {
